5ead9ecb39584f9513be1e311563201832b371f7
Simondemonte <simondemonte8@gmail.com>
2017-09-27 10:15:09 -0700

    Level 12
    Update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,3 +38,9 @@ Route::get('/read', function(){
 			echo $post->title."<br/>";
 		}
 });
+
+Route::get('/update', function(){
+		$user = User::find(1);
+		
+		$user->posts()->whereId(1)->update(['title'=>'I love laravel so much', 'body'=>'this is awesome']);
+});
