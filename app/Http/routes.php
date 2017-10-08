50ef69f33ac428acca7c350e27cc705f98e7c81e
Simondemonte <simondemonte8@gmail.com>
2017-10-08 00:41:08 -0700

    Level 13
    Deleting Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -50,3 +50,11 @@ Route::get('/update', function(){
 		}
 	}
 });
+
+Route::get('/delete', function(){
+	$user = User::findOrFail(1);
+	
+	foreach($user->roles as $role){
+		$role->whereId(3)->delete();
+	}
+});
