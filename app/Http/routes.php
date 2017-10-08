475c5a0ba435b9d8c17796dfc2a5773cfb774ab1
Simondemonte <simondemonte8@gmail.com>
2017-10-08 01:58:10 -0700

    Level 15
    Creating new Laravel Installation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,37 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-use App\Staff;
-use App\Photo;
+
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/create', function(){
-	$staff = Staff::find(1);
-	
-	$staff->photos()->create(['path'=>'example.jpg']);
-});
-
-Route::get('/read', function(){
-	$staff = Staff::findOrFail(1);
-	
-	foreach($staff->photos as $photo){
-		return $photo->path;
-	}
-});
-
-Route::get('/update', function(){
-	$staff = Staff::findOrFail(1);
-	
-	$photo = $staff->photos()->whereId(1)->first();
-	$photo->path = "Updated.jpg";
-	$photo->save();
-});
-
-Route::get('/delete', function(){
-	$staff = Staff::findOrFail(1);
-	
-	//$staff->photos()->whereName('bad_photo.jpg')->delete(); //alternative
-	$staff->photos()->delete();
 });
