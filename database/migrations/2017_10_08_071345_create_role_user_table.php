3b94b9c1b0094d5c3ff03bb37dc0bf7e79b0ad47
Simondemonte <simondemonte8@gmail.com>
2017-10-07 23:29:25 -0700

    Level 13
    Database Configuration and Migration

--- database/migrations/2017_10_08_071345_create_role_user_table.php
+++ database/migrations/2017_10_08_071345_create_role_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateRoleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->increments('id');
 			$table->integer('user_id')->unsigned()->nullable()->index();
-			$table->string('title');
-			$table->text('body');
+			$table->integer('role_id')->unsigned()->nullable()->index();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('posts');
+        Schema::drop('role_user');
     }
 }
