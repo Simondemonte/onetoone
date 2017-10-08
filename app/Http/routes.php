94cbb3bcb5e9a9444a9b6923a91e8f1594df3702
Simondemonte <simondemonte8@gmail.com>
2017-10-07 23:56:21 -0700

    Level13
    Inserting Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,15 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\Role;
+use App\User;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/create', function(){
+		$user = User::find(1);
+		
+		$role = new Role(['name'=>'Administrator']);
+		$user->roles()->save($role);
 });
