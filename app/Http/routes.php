24f7dc53e069488e855197bcdcbe3690a7cbbd7b
Simondemonte <simondemonte8@gmail.com>
2017-10-08 00:36:03 -0700

    Level 13
    Updating Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,3 +38,15 @@ Route::get('read', function(){
 	}
 });
 
+Route::get('/update', function(){
+	$user = User::findOrFail(1);
+	
+	if($user->has('roles')){
+		foreach($user->roles as $role){
+			if($role->name == 'Administrator'){
+				$role->name == 'subscriber';
+				$role->save();
+			}
+		}
+	}
+});
