2fe25199975bea87281bad6e0e876c89ba9e14cb
Simondemonte <simondemonte8@gmail.com>
2017-10-11 08:38:36 -0700

    Level 17
    Installing Package and Testing

---
+++ laracms/resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+
+
+@section('content')
+
+	<!--form action="/posts"method="post">-->
+	{!! Form::open() !!}
+		<input type="text" name="title"/>
+		{{csrf_field()}}
+		<input type="submit" name="submit"/>
+	</form>
+	
+@stop
+
+
+@section('footer')
+
+
+@stop
