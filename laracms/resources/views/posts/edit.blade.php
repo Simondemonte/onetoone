e04ab2c9a2af46c7b9a531890b92a2e0d27ef92f
Simondemonte <simondemonte8@gmail.com>
2017-10-11 08:52:26 -0700

    Level 17
    Modifying Edit & Delete Forms with the Form Package

--- laracms/resources/views/posts/edit.blade.php
+++ laracms/resources/views/posts/edit.blade.php
@@ -6,20 +6,26 @@
 
 	<h1>Edit Post</h1>
 	
-	<form action="/posts/{{$post->id}}"method="post">
+	<!--form action="/posts/{{$post->id}}"method="post">
 		<input type="hidden" name="_method" value="PUT" />
 		<input type="text" name="title" value="{{$post->title}}"/>
 		{{csrf_field()}}
 		<input type="submit" name="submit" />
-	</form>
-	
+	</form>-->
+		
+		{!! Form::model($post, ['method'=>'PATCH', 'action'=>['PostsController@update', $post->id]]) !!}
+		
+		{!! Form::label('title', 'Title:') !!}
+			{!! Form::text('title', null, ['class'=>'form-control']) !!}
+			
+			{!! Form::submit('Update Post', ['class'=>btn btn-info']) !!}
+		
+		{!! Form:close()!!}
 	<form action="/post/{{$post->id}}"method="post">
 		<input type="hidden" name="_method" value="DELETE" />
 		{{csrf_field()}}
 		<input type="submit" value="DELETE"/>
 	</form>
-		
-	
 @stop
 
 
