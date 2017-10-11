bee46bfd58aec249a55094c926158013b66880c2
Simondemonte <simondemonte8@gmail.com>
2017-10-11 06:46:40 -0700

    Level 16
    Setting Up Migration and Relations

--- laracms/database/migrations/2017_10_10_135026_create_posts_table.php
+++ laracms/database/migrations/2017_10_10_135026_create_posts_table.php
@@ -14,7 +14,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('name');
+			$table->integer('user_id')->unsigned();
+			$table->string('title');
+			$table->text('content');
             $table->timestamps();
         });
     }
